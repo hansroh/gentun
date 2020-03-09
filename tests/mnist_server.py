9f7938688eef541bef4ff6cd957374570d1f330d
ahmadmobeen <ahmadmobeen24@gmail.com>
2020-03-09 17:54:18 +0900

    from server. if broken revert this

--- tests/mnist_server.py
+++ tests/mnist_server.py
@@ -14,10 +14,10 @@ if __name__ == '__main__':
     from gentun import RussianRouletteGA, DistributedPopulation, GeneticCnnIndividual
 
     pop = DistributedPopulation(
-        GeneticCnnIndividual, size=20, crossover_rate=0.3, mutation_rate=0.1,
+        GeneticCnnIndividual, size=4, crossover_rate=0.3, mutation_rate=0.1,
         additional_parameters={
             'kfold': 5, 'epochs': (20, 4, 1), 'learning_rate': (1e-3, 1e-4, 1e-5), 'batch_size': 32
-        }, maximize=True, host='223.195.37.85', user='test', password='test'
+        }, maximize=True, host='localhost', user='test', password='test'
     )
     ga = RussianRouletteGA(pop, crossover_probability=0.2, mutation_probability=0.8)
     ga.run(50)
